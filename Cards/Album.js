e857ffdd020c1021cd213ae8040b88cc438d9c2f
Varun Srivastava <48484555+VarunSrivastava19@users.noreply.github.com>
2021-02-15 12:06:56 +0530

    Card

---
+++ Cards/Album.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import RoomIcon from '@material-ui/icons/Room';
+import PrimarySearchAppBar from './AppBar/AppBar';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="#FoodzApp">
+        FoodzApp
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Album() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      {/*AppBar Component*/}
+      <PrimarySearchAppBar/>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              {/*Pull from DB: Name of user according to his username using id of user*/}
+              John Doe
+            </Typography>
+            <Typography variant="p" align="center" color="textSecondary" paragraph>
+              <RoomIcon/> 
+              {/*Pune, MH, IN*/}
+              New Delhi, IN
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Edit Profile
+                  </Button>
+                </Grid>
+                {/* <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid> */}
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        {/* End hero unit */}
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {/*Pull Past orders from User's DB using user's ID*/}
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Restaurant 1
+                    </Typography>
+                    <Typography variant="p" align="left" color="textSecondary" paragraph>
+                    ORDER NUMBER
+                    </Typography>
+                    <Typography variant="p" align="left" color="textPrimary" paragraph>
+                    #12345
+                    </Typography>
+                    <Typography variant="p" align="left" color="textSecondary" paragraph>
+                      TOTAL AMOUNT
+                    </Typography>
+                    <Typography variant="p" align="left" color="textPrimary" paragraph>
+                      ₹449.75
+                    </Typography>                
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" variant="contained" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" variant="outlined" color="secondary">
+                      Re-order
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
